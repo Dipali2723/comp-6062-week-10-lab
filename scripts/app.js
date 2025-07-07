7f8110e52ec8a3b02f4d6fed64f6b0464a3691d0
Dipali276 <dipalighogharica@gmail.com>
2025-07-07 18:46:12 -0400

    full name

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,18 @@ console.log('Loaded!');
 const app=Vue.createApp({
     data(){
         return{
-            title: 'Week 10 Lab'
+            title: 'Week 10 Lab',
+            user: {
+            firstName: 'Dipali',
+            lastName: 'Ghoghari'
+            }
         }
         userInput: ''
+    },
+    computed: {
+        FullName(){
+            return `${this.user.firstName} ${this.user.lastName}`;
+        }
     }
 });
 
