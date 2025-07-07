bff453f0bce4da6d799b2995b2f27ff08641240d
Dipali276 <dipalighogharica@gmail.com>
2025-07-07 19:37:04 -0400

    full change

--- scripts/app.js
+++ scripts/app.js
@@ -5,17 +5,31 @@ const app=Vue.createApp({
         return{
             title: 'Week 10 Lab',
             user: {
-            firstName: 'Dipali',
-            lastName: 'Ghoghari'
-            }
+            firstName: '',
+            lastName: ''
+            },
+            count: 0,
+            PriceperItem:10,
+            quantity: 0
         }
-        userInput: ''
     },
     computed: {
         FullName(){
             return `${this.user.firstName} ${this.user.lastName}`;
+        },
+        totalprice(){
+            return this.quantity * this.PriceperItem
+        }
+    
+    },
+    methods:{
+        clearField(){
+            this.user.firstName='';
+            this.user.lastName='';
+            this.quantity= 0;
         }
     }
+    
 });
 
 app.mount('#app');
